6c6671431dc68218f796a494f9a04f816b403764
root <1821231139@qq.com>
2022-09-20 12:35:18 +0800

    hot-fix commit2

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("GitHub Test2");
         System.out.println("GitHub Test3");
         System.out.println("GitHub Test4");
+        System.out.println("hot-fix Test");
     }
 }
